9446724f5db9cf8c4305d7571b08043767c4b908
Baran Gozetir <barangozetir@gmail.com>
2023-01-04 00:48:59 +0300

    cors erros fixed

--- server/index.js
+++ server/index.js
@@ -1,5 +1,6 @@
 import express from "express";
 import dotenv from "dotenv";
+import cors from "cors";
 import { graphqlHTTP } from "express-graphql";
 import schema from "./schema/schema.js";
 import { connectDB } from "./config/db.js";
@@ -11,6 +12,7 @@ const port = process.env.PORT || 5000;
 const app = express();
 
 connectDB();
+app.use(cors());
 
 app.use(
   "/graphql",
