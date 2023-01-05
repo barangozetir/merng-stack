9419e4bac22a21c77e3125d0d8091954e6df389d
Baran Gozetir <barangozetir@gmail.com>
2023-01-05 03:16:21 +0300

    Projects components

---
+++ client/src/components/Projects.jsx
@@ -0,0 +1,27 @@
+import React from 'react'
+import Spinner from './Spinner'
+import {useQuery} from "@apollo/client"
+import {GET_PROJECTS} from "../queries/projectQueris.js"
+import ProjectCard from './ProjectCard'
+
+const Projects = () => {
+    const {loading, error, data } = useQuery(GET_PROJECTS)
+    if(loading) return <Spinner />;
+    if(error) return <p>Error</p>
+
+  return (
+    <>
+    {data.projects ? (
+        <div className='row-mt-4'>
+            {
+                data.projects.map((project)=>
+                   <ProjectCard key={project.id} project={project} />
+                )
+            }
+        </div>
+    ) : (<p>No Projects</p>)}
+    </>
+  )
+}
+
+export default Projects
