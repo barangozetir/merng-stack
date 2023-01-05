aca93f460cfa52b4792a2d17c7cb780ff271051d
Baran Gozetir <barangozetir@gmail.com>
2023-01-05 03:15:27 +0300

    delete client

--- client/src/components/ClientRow.jsx
+++ client/src/components/ClientRow.jsx
@@ -1,13 +1,31 @@
 import React from 'react'
 import {FaTrash} from "react-icons/fa"
+import {useMutation} from "@apollo/client"
+import {DELETE_CLIENT} from "../mutations/clientMutations"
+import {GET_CLIENTS} from "../queries/qlientQueries"
+
+
 
 const ClientRow = ({client}) => {
+  const [deleteClient] = useMutation(DELETE_CLIENT, {
+    variables: {id:client.id},
+    
+    // when you delete see on front-end
+    update(cache, {data: {deleteClient}}){
+      const {clients} = cache.readQuery({query:GET_CLIENTS});
+      cache.writeQuery({
+        query:GET_CLIENTS,
+        data: {clients:clients.filter(client=>client.id!==deleteClient.id)}
+      })
+    }
+  })
+
   return (
     <tr>
         <td>{client.name}</td>
         <td>{client.email}</td>
         <td>{client.phone}</td>
-        <td><button className="btn btn-danger bt-sm"><FaTrash /></button></td>
+        <td><button className="btn btn-danger bt-sm" onClick={deleteClient}><FaTrash /></button></td>
     </tr>
   )
 }
