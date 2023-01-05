22ab0232cafbb7a3958139918b11992b851b771f
Baran Gozetir <barangozetir@gmail.com>
2023-01-05 03:17:05 +0300

    cache warning fixed, components added

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,32 @@
 import { ApolloProvider, ApolloClient, InMemoryCache } from "@apollo/client";
 import Header from "./components/Header";
 import Client from "./components/Client";
+import AddClientModal from "./components/AddClientModal";
+import Projects from "./components/Projects";
+
+// for warning in console
+const cache = new InMemoryCache({
+  typePolicies: {
+    Query: {
+      fields: {
+        clients: {
+          merge(existing, incoming) {
+            return incoming;
+          },
+        },
+        projects: {
+          merge(existing, incoming) {
+            return incoming;
+          },
+        },
+      },
+    },
+  },
+});
 
 const client = new ApolloClient({
   uri: "http://localhost:5000/graphql",
-  cache: new InMemoryCache(),
+  cache,
 });
 
 function App() {
@@ -13,6 +35,8 @@ function App() {
       <ApolloProvider client={client}>
         <Header />
         <div className="container">
+          <Projects />
+          <AddClientModal />
           <Client />
         </div>
       </ApolloProvider>
